6cd41e3abb3991ba49ea24a62ed428fd9d78ac6c
gustavoeochoa@gmail.com <gustavoeochoa@gmail.com>
2023-07-12 18:31:51 -0300

    npm installations

--- next.config.js
+++ next.config.js
@@ -1,14 +1,11 @@
 module.exports = {
   reactStrictMode: true,
   env:{
-    int_frmwk5 : 'https://local.com/tdi/Intranet/Aplicaciones/',
-    ext_frmwk5 : 'https://local.com/tdi/Extranet/Aplicaciones/',
-    fmk_frmwk6 : 'http://localhost/frmwk/framework/',
-    int_frmwk6 : 'http://localhost/frmwk/intranet/',
-    ext_frmwk6 : 'http://localhost/frmwk/extranet/',
-    frmwk6_title :'Tecnología y Desarrollo Informatico',
-    page_title: 'TDI FRAMEWORK',
-    enviroment: 'production',
+    int : 'http://localhost/app/intranet',
+    ext : 'http://localhost/app/extranet',
+    title :'Gurthang Dashboard Themplate',
+    page_title: 'Gurthang Theme',
+    enviroment: 'development',
     maxAge: 2592000
   },
   async redirects() {
